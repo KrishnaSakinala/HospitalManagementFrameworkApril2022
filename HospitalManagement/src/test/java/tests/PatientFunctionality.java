50e3bc2d5219ae5eaf25c3ca023d242fe860d288
sakinala <ep.krishna@gmail.com>
2022-06-13 07:19:15 +0530

    Adding Patient Creation Test Case

--- HospitalManagement/src/test/java/tests/PatientFunctionality.java
+++ HospitalManagement/src/test/java/tests/PatientFunctionality.java
@@ -1,2 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
 public class PatientFunctionality {
+	
+	@Test
+	public void verifyPatientCreation() {
+		System.out.println("Creating a new Patient");
+	}
 }
